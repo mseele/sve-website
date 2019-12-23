5a57af9e4e9cdf02c2eff119c0c2a4374902f734
mseele <mseele@gmail.com>
2019-12-23 17:11:33 +0100

    add news title

--- components/sections/News.vue
+++ components/sections/News.vue
@@ -1,4 +1,12 @@
 <template>
-  <!-- placeholder -->
-  <div style="height:500px">Aktuelles</div>
+  <div class="section">
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <h2>{{ $t('news.title') }}</h2>
+        </v-col>
+        <v-col cols="12" style="height:500px"><!-- todo --></v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
