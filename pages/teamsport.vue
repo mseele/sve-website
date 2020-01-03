f6c8af174246a09cb398b492c42f3661ec46055d
Sebastian Lazar <bennibr@SebastiLazarMBP.fritz.box>
2020-01-03 10:38:31 +0100

    einführungstext geändert

--- pages/teamsport.vue
+++ pages/teamsport.vue
@@ -2,11 +2,7 @@
   <div>
     <section id="fussball" class="section">
       <v-container>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet.
+        test sebastian
       </v-container>
     </section>
     <section id="herren" class="section_alt">
