eb38955e169645fcc3c9bec9908e26daa7d05915
mseele <mseele@gmail.com>
2020-01-07 17:10:05 +0100

    Revert "einführungstext geändert"
    This reverts commit f6c8af174246a09cb398b492c42f3661ec46055d.

--- pages/teamsport.vue
+++ pages/teamsport.vue
@@ -2,7 +2,11 @@
   <div>
     <section id="fussball" class="section">
       <v-container>
-        test sebastian
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet.
       </v-container>
     </section>
     <section id="herren" class="section_alt">
