c6bd90791396ddbc680148139e57a215d7b21d3b
mseele <mseele@gmail.com>
2020-01-15 20:43:13 +0100

    add fullscreen viewer

--- components/common/ImageViewer.vue
+++ components/common/ImageViewer.vue
@@ -5,6 +5,7 @@
       :lazy-src="images[0].lazy"
       :src="images[0].src"
       :height="height"
+      @click="index = 0"
     ></v-img>
     <v-carousel
       v-else
@@ -18,9 +19,48 @@
         :key="imageIndex"
         :lazy-src="image.lazy"
         :src="image.src"
+        @click="index = imageIndex"
       >
       </v-carousel-item>
     </v-carousel>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-sheet color="black">
+        <v-img
+          v-if="images.length == 1"
+          :lazy-src="images[0].lazy"
+          :src="viewerImage(images[0])"
+          height="100vh"
+          contain
+        ></v-img>
+        <v-carousel
+          v-else
+          hide-delimiter-background
+          show-arrows-on-hover
+          height="100vh"
+          :value="index"
+        >
+          <v-carousel-item
+            v-for="(image, imageIndex) in images"
+            :key="imageIndex"
+          >
+            <v-img
+              :lazy-src="image.lazy"
+              :src="image.big ? image.big : image.src"
+              height="100vh"
+              contain
+            ></v-img>
+          </v-carousel-item>
+        </v-carousel>
+        <v-btn absolute top right icon dark @click="index = null">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-sheet>
+    </v-dialog>
   </div>
 </template>
 
@@ -34,6 +74,23 @@ export default {
     images: {
       type: Array,
       default: () => []
+    }
+  },
+  data() {
+    return {
+      index: null
+    }
+  },
+  computed: {
+    dialog: {
+      get() {
+        return this.index != null
+      }
+    }
+  },
+  methods: {
+    viewerImage(image) {
+      return image.big ? image.big : image.src
     }
   }
 }
