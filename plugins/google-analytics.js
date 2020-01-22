3e2edcabb4f37dbe19dd300f85f3fc3bf1f7349a
mseele <mseele@gmail.com>
2020-01-22 21:58:49 +0100

    remove unused plugin js

--- plugins/google-analytics.js
+++
@@ -1,7 +0,0 @@
-const getGDPR = localStorage.getItem('gdpr:accepted')
-
-if (typeof getGDPR !== 'undefined' && getGDPR === 'true') {
-  this.$ga.enable()
-} else if (typeof getGDPR !== 'undefined' && getGDPR === 'false') {
-  this.$ga.disable()
-}
