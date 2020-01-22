de3e70d738c795deb804808838f00951e2db2153
mseele <mseele@gmail.com>
2020-01-22 20:57:09 +0100

    move data into store

--- pages/teamsport.vue
+++ pages/teamsport.vue
@@ -24,7 +24,7 @@
         </v-row>
         <v-row justify="center">
           <v-col
-            v-for="(item, index) in herren"
+            v-for="(item, index) in teams.herren"
             :key="index"
             cols="12"
             sm="6"
@@ -51,7 +51,7 @@
         </v-row>
         <v-row justify="center">
           <v-col
-            v-for="(item, index) in damen"
+            v-for="(item, index) in teams.damen"
             :key="index"
             cols="12"
             sm="6"
@@ -79,7 +79,7 @@
         </v-row>
         <v-row justify="center">
           <v-col
-            v-for="(item, index) in jugend_herren"
+            v-for="(item, index) in teams.jugend_herren"
             :key="index"
             cols="12"
             sm="6"
@@ -107,7 +107,7 @@
         </v-row>
         <v-row justify="center">
           <v-col
-            v-for="(item, index) in jugend_damen"
+            v-for="(item, index) in teams.jugend_damen"
             :key="index"
             cols="12"
             sm="6"
@@ -135,7 +135,7 @@
         </v-row>
         <v-row justify="center">
           <v-col
-            v-for="(item, index) in kinder"
+            v-for="(item, index) in teams.kinder"
             :key="index"
             cols="12"
             sm="6"
@@ -161,7 +161,7 @@
         </v-row>
         <v-row justify="center">
           <v-col
-            v-for="(item, index) in volleyball"
+            v-for="(item, index) in teams.volleyball"
             :key="index"
             cols="12"
             sm="6"
@@ -190,149 +190,12 @@ export default {
         { title: 'Jugendfussball (weiblich)', to: '#jugend-weiblich' },
         { title: 'Kinderfussball', to: '#kinder' },
         { title: 'Volleyball', to: '#volleyball' }
-      ],
-      herren: [
-        {
-          team: '1. Mannschaft',
-          league: 'Kreisliga A2',
-          contact: 'Abteilungsleiter Raphael Sickler',
-          contactLink: '/kontakt',
-          teamID: '011MID6OM0000000VTVG0001VTR8C1K7'
-        },
-        {
-          team: '2. Mannschaft',
-          league: 'Kreisliga B1',
-          contact: 'Abteilungsleiter Raphael Sickler',
-          contactLink: '/kontakt',
-          teamID: '01A1LJM17C000000VV0AG80NVSEJ47CH'
-        }
-      ],
-      damen: [
-        {
-          team: '1. Mannschaft',
-          league: 'Verbandsliga',
-          contact: 'Abteilungsleiterin Samantha Becker',
-          contactLink: '/kontakt',
-          teamID: '011MIBTDHG000000VTVG0001VTR8C1K7'
-        },
-        {
-          team: '2. Mannschaft',
-          league: 'Bezirksliga',
-          contact: 'Abteilungsleiterin Christina Saile',
-          contactLink: '/kontakt',
-          teamID: '01H9SAS6NS000000VV0AG811VU6HO2AK'
-        }
-      ],
-      jugend_herren: [
-        {
-          team: 'A-Junioren SGM SV Eutingen',
-          league: 'Leistungsstaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '012GED15C4000000VV0AG811VT1UN0HI'
-        },
-        {
-          team: 'B-Junioren SGM SV Vollmaringen',
-          league: 'Bezirksstaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '01SM9K6ICS000000VS548985VU8O1RK1'
-        },
-        {
-          team: 'C-Junioren 1 SGM VfL Hochdorf',
-          league: 'Bezirksstaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '01L6RV7RVO000000VV0AG811VS9BQ5HH'
-        },
-        {
-          team: 'C-Junioren 2 SGM VfL Hochdorf',
-          league: 'Bezirksstaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '023UL6T654000000VS548985VVRPGK5E'
-        },
-        {
-          team: 'D-Junioren 1 SGM SV Vollmaringen',
-          league: 'Leistungsstaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '027H96HED8000000VS5489B1VUA37ON8'
-        },
-        {
-          team: 'D-Junioren 2 SGM SV Vollmaringen',
-          league: '7er Kreisstaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '027H9744D8000000VS5489B1VUA37ON8'
-        }
-      ],
-      jugend_damen: [
-        {
-          team: 'B-Juniorinnen 1',
-          league: 'EnBW-Oberliga',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '011MI98PJG000000VTVG0001VTR8C1K7'
-        },
-        {
-          team: 'B-Juniorinnen 2',
-          league: 'Bezirksstaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '011MIE4SVS000000VTVG0001VTR8C1K7'
-        },
-        {
-          team: 'C-Juniorinnen',
-          league: 'Qualistaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '027ISEG2HS000000VS5489B1VUA37ON8'
-        },
-        {
-          team: 'D-Juniorinnen 1 SGM SV Eutingen',
-          league: 'Qualistaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '0240ABCB3K000000VS548985VV3HBH6M'
-        },
-        {
-          team: 'D-Juniorinnen 2 SGM SV Eutingen',
-          league: 'Qualistaffel',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '0240ABO73G000000VS548985VV3HBH6M'
-        }
-      ],
-      kinder: [
-        {
-          team: 'E-Junioren',
-          league: 'Freundschaftsstaffel 2',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt',
-          teamID: '011MICV3FO000000VTVG0001VTR8C1K7'
-        },
-        {
-          team: 'F-Junioren',
-          league: '-',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt'
-        },
-        {
-          team: 'Bambini',
-          league: '-',
-          contact: 'Jugendleiterin Diana Akermann',
-          contactLink: '/kontakt'
-        }
-      ],
-      volleyball: [
-        {
-          team: 'Erwachsenen-Hobbygruppe',
-          league: '-',
-          contact: 'Rainer Busch',
-          contactLink: '/kontakt'
-        }
       ]
+    }
+  },
+  computed: {
+    teams() {
+      return this.$store.state.teams
     }
   },
   head() {
---
+++ store/teams.js
@@ -0,0 +1,144 @@
+export const state = () => ({
+  herren: [
+    {
+      team: '1. Mannschaft',
+      league: 'Kreisliga A2',
+      contact: 'Abteilungsleiter Raphael Sickler',
+      contactLink: '/kontakt',
+      teamID: '011MID6OM0000000VTVG0001VTR8C1K7'
+    },
+    {
+      team: '2. Mannschaft',
+      league: 'Kreisliga B1',
+      contact: 'Abteilungsleiter Raphael Sickler',
+      contactLink: '/kontakt',
+      teamID: '01A1LJM17C000000VV0AG80NVSEJ47CH'
+    }
+  ],
+  damen: [
+    {
+      team: '1. Mannschaft',
+      league: 'Verbandsliga',
+      contact: 'Abteilungsleiterin Samantha Becker',
+      contactLink: '/kontakt',
+      teamID: '011MIBTDHG000000VTVG0001VTR8C1K7'
+    },
+    {
+      team: '2. Mannschaft',
+      league: 'Bezirksliga',
+      contact: 'Abteilungsleiterin Christina Saile',
+      contactLink: '/kontakt',
+      teamID: '01H9SAS6NS000000VV0AG811VU6HO2AK'
+    }
+  ],
+  jugend_herren: [
+    {
+      team: 'A-Junioren SGM SV Eutingen',
+      league: 'Leistungsstaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '012GED15C4000000VV0AG811VT1UN0HI'
+    },
+    {
+      team: 'B-Junioren SGM SV Vollmaringen',
+      league: 'Bezirksstaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '01SM9K6ICS000000VS548985VU8O1RK1'
+    },
+    {
+      team: 'C-Junioren 1 SGM VfL Hochdorf',
+      league: 'Bezirksstaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '01L6RV7RVO000000VV0AG811VS9BQ5HH'
+    },
+    {
+      team: 'C-Junioren 2 SGM VfL Hochdorf',
+      league: 'Bezirksstaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '023UL6T654000000VS548985VVRPGK5E'
+    },
+    {
+      team: 'D-Junioren 1 SGM SV Vollmaringen',
+      league: 'Leistungsstaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '027H96HED8000000VS5489B1VUA37ON8'
+    },
+    {
+      team: 'D-Junioren 2 SGM SV Vollmaringen',
+      league: '7er Kreisstaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '027H9744D8000000VS5489B1VUA37ON8'
+    }
+  ],
+  jugend_damen: [
+    {
+      team: 'B-Juniorinnen 1',
+      league: 'EnBW-Oberliga',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '011MI98PJG000000VTVG0001VTR8C1K7'
+    },
+    {
+      team: 'B-Juniorinnen 2',
+      league: 'Bezirksstaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '011MIE4SVS000000VTVG0001VTR8C1K7'
+    },
+    {
+      team: 'C-Juniorinnen',
+      league: 'Qualistaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '027ISEG2HS000000VS5489B1VUA37ON8'
+    },
+    {
+      team: 'D-Juniorinnen 1 SGM SV Eutingen',
+      league: 'Qualistaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '0240ABCB3K000000VS548985VV3HBH6M'
+    },
+    {
+      team: 'D-Juniorinnen 2 SGM SV Eutingen',
+      league: 'Qualistaffel',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '0240ABO73G000000VS548985VV3HBH6M'
+    }
+  ],
+  kinder: [
+    {
+      team: 'E-Junioren',
+      league: 'Freundschaftsstaffel 2',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt',
+      teamID: '011MICV3FO000000VTVG0001VTR8C1K7'
+    },
+    {
+      team: 'F-Junioren',
+      league: '-',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt'
+    },
+    {
+      team: 'Bambini',
+      league: '-',
+      contact: 'Jugendleiterin Diana Akermann',
+      contactLink: '/kontakt'
+    }
+  ],
+  volleyball: [
+    {
+      team: 'Erwachsenen-Hobbygruppe',
+      league: '-',
+      contact: 'Rainer Busch',
+      contactLink: '/kontakt'
+    }
+  ]
+})
