6985ec52a927f3ca5fa94c008ad331b8084865d3
mseele <mseele@gmail.com>
2020-02-29 15:25:06 +0100

    implement leitbild v1

--- pages/leitbild.vue
+++ pages/leitbild.vue
@@ -1,9 +1,93 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      leitbild
-    </v-flex>
-  </v-layout>
+  <div>
+    <section id="kunstrasen" class="section">
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <h2>LEITBILD</h2>
+          </v-col>
+          <v-col cols="12" class="title">
+            SV Eutingen – #mehralseinverein.
+          </v-col>
+          <v-col cols="12" class="title">
+            Wir stiften gesellschaftliche Teilhabe durch Sport und Begegnung.
+          </v-col>
+          <v-col cols="12" class="subtitle-1">
+            Unser sportliches und kulturelles Angebot ermöglicht
+            gesellschaftliche Teilhabe. Bei uns begegnen sich Menschen. Dadurch
+            entstehen Freundschaften, Netzwerke, Perspektiven, Teilhabe. Durch
+            unser Sportangebot und verschiedene Treffpunkte kommen Menschen
+            zusammen und erleben eine schöne Zeit. Freude und Gemeinschaft
+            werden bei uns großgeschrieben. Wir lieben die Freudenschreie
+            ausgelassener Kinder, den Torjubel für unsere Teams, den Treffpunkt
+            durch unsere Gaststätte Auszeit, die Abendsonne am Lagerfeuer.<br />
+            <br />
+            Wir entfalten die Talente unserer Mitglieder, packen an und trauen
+            einander etwas zu. Durch Reflexion, ehrliches Feedback, Aus- und
+            Weiterbildung werden wir besser. Unser Verein entwickelt sich
+            gesund. Was sich seit 1947 bewährt hat, führen wir fort und steuern
+            auch neue Ufer an.<br />
+            Unser Verein bringt sich aktiv in Eutingen ein. Durch unser Sport-,
+            Kultur- und Veranstaltungsangebot schaffen wir Heimat.<br />
+            Bei uns wird nicht nach Herkunft, Geschlecht oder Alter gefragt,
+            sondern nach der gemeinsamen Motivation, etwas auf die Beine zu
+            stellen.<br />
+            Uns ist jeder Beitrag für das große Ganze wichtig.<br />
+            <br />
+            Wertschätzung wird bei uns spürbar:
+            <ul>
+              <li>
+                Für aktive Kinder, Jugendliche, Spielerinnen und Spieler, die
+                wir am Spielfeldrand anfeuern.
+              </li>
+              <li>
+                Für passive Mitglieder und Macher, die zentrale Ehrenämter
+                ausüben, unsere Infrastruktur in Schuss halten und nach getaner
+                Arbeit Gemeinschaft genießen.
+              </li>
+              <li>
+                Für unsere Pächter, Partner und Dienstleister, mit denen wir
+                Zusammenarbeit leben.
+              </li>
+            </ul>
+          </v-col>
+          <v-col cols="12" class="title">
+            Teamgeist, Fairness, Leistungsbereitschaft – Grundsätze unseres
+            Sportangebots.
+          </v-col>
+          <v-col cols="12" class="subtitle-1">
+            Unseren Mitgliedern und Kunden bieten wir aktive sportliche
+            Betätigung.<br />
+            Dadurch stärken sie ihre mentale und körperliche Gesundheit, erleben
+            Gemeinschaftsgefühl und Erfolge in Teams sowie Wettkampf. Durch
+            Niederlagen lernen wir den fairen Umgang damit.<br />
+            <br />
+            Unser Sportangebot ist breit. Es reicht von offenen
+            Individualangeboten, über Teamsport im Hobby-, Amateur- und
+            Leistungsbereich.<br />
+            Fußball ist unser Schwerpunkt – für Kinder, Jugendliche, Damen und
+            Herren.<br />
+            Sportliche Ziele, die wir mit gesundem und fairem Einsatz sowie
+            Ehrgeiz erreichen können, wollen wir auch erreichen.<br />
+            Statt phasenweiser Liga- und Tabellenplatzsituation steht für uns im
+            Vordergrund, das Beste aus den Teams herauszuholen und Wir-Gefühl
+            möglich zu machen.<br />
+            <br />
+            Durch Teamgeist, Kameradschaft und gesunder Leistungsbereitschaft
+            binden wir Kinder, Jugendliche, Spielerinnen und Spieler nachhaltig
+            an unseren Verein. Dies sichert die Zukunft.<br />
+            Für unsere Sportangebote und Treffpunkte stellen wir eine sehr gute
+            Infrastruktur zur Verfügung. Diese pflegen wir und gehen mit
+            Ressourcen nachhaltig und schonend um.
+          </v-col>
+          <v-col cols="12" class="title">
+            Der SV Eutingen ist ein ehrenamtlicher Verein – und
+            #mehralseinverein.
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+  </div>
 </template>
 
 <script>
