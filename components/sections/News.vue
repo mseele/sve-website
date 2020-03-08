d79fdae1bcf66d039d883d286ce0458537eba164
mseele <mseele@gmail.com>
2020-03-08 17:50:04 +0100

    implement news section

--- components/sections/News.vue
+++ components/sections/News.vue
@@ -5,8 +5,28 @@
         <v-col cols="12">
           <h2>AKTUELLES</h2>
         </v-col>
-        <v-col cols="12" style="height:500px"><!-- todo --></v-col>
+        <v-col v-for="(item, index) in items()" :key="index" cols="12">
+          <v-card>
+            <v-card-title>
+              {{ item.title }}
+            </v-card-title>
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <v-card-text v-html="item.text"></v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
+
+<script>
+const maxItems = 4
+
+export default {
+  methods: {
+    items() {
+      return this.$store.state.news.nodes.slice(0, maxItems)
+    }
+  }
+}
+</script>
