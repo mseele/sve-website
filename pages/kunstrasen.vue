31fe3c42855a454323a9c8b423fe23ee735738d7
mseele <mseele@gmail.com>
2020-06-08 19:22:42 +0200

    improve kunstrasen content
    fix https://github.com/mseele/sve-website/issues/83

--- pages/kunstrasen.vue
+++ pages/kunstrasen.vue
@@ -6,13 +6,15 @@
           <v-col cols="12">
             <h2>VERMIETUNG KUNSTRASENPLATZ</h2>
           </v-col>
-          <v-col cols="12" class="subtitle-1"
-            >Mit unserem “Projekt Kunstrasen” wurde eine top moderne
-            Kunstrasenanlage (Groß- und Kleinspielfeld) inklusive Flutlicht
-            errichtet. Diese Anlage bietet optimale Spielbedingungen auch in
+          <v-col cols="12" class="subtitle-1">
+            Seit 2018 verfügen wir über eine hochmoderne Sportanlage mit einem
+            Kunstrasengroß- und Kleinspielfeld inklusive Flutlicht. Der
+            Sportbereich mit vier geschlechtergetrennten Kabinen und separaten
+            Räumen für Schiedsrichter bietet optimale Spielbedingungen auch in
             Zeiten von Schnee, Regen oder Frost.<br />
-            Wir würden uns freuen, auch Eurem Verein / Eurer Mannschaft diese
-            Möglichkeit zur Verfügung stellen zu können.
+            Gerne stellen wir Eurem Team den großen Kunstrasenplatz (100 x 70
+            Meter) für eine Trainings- oder Spieleinheit zur Verfügung, wenn die
+            Anlage nicht durch unsere vielen eigenen Teams genutzt wird.
           </v-col>
         </v-row>
       </v-container>
@@ -25,36 +27,47 @@
           </v-col>
           <v-col cols="12" class="subtitle-1">
             <div class="font-weight-bold">
-              Nutzungsgebühren / Spiel: 180 € (inkl. MwSt.)
+              Tarife für Sportvereine außerhalb der Gemeinde Eutingen im Gäu:
             </div>
-            <div>
-              Die Platzgebühren sind unmittelbar vor Benutzung in bar zu
-              errichten. Ihr erhaltet eine Quittung.
-            </div>
+            <ul>
+              <li>
+                Wintertarif ohne Flutlicht: von Oktober bis März für Spiel und
+                Trainingseinheit (90 Minuten) zwischen 10 und 16 Uhr: 110 Euro
+                pro Team
+              </li>
+              <li>
+                Wintertarif mit Flutlicht: von Oktober bis März für Spiel und
+                Trainingseinheit (90 Minuten) zwischen 16 und 20 Uhr: 125 Euro
+                pro Team
+              </li>
+              <li>
+                Sommertarif ohne Flutlicht: von April bis September für Spiel
+                und Trainingseinheit (90 Minuten): 110 Euro pro Team
+              </li>
+            </ul>
           </v-col>
           <v-col cols="12" class="subtitle-1">
             <div class="font-weight-bold">
-              In der Nutzungsgebühr enthalten:
+              Tarife für Sportvereine innerhalb der Gemeinde Eutingen im Gäu:
             </div>
-            <div>
-              <ul>
-                <li>
-                  die Benutzung des Platzes, der Umkleiden und Sanitäranlagen
-                </li>
-                <li>
-                  Flutlicht
-                </li>
-              </ul>
-            </div>
+            <ul>
+              <li>
+                bei Inanspruchnahme der Umkleidekabinen, Sanitärräume und
+                Flutlichtanlage pro Spiel oder Trainingseinheit (90 Minuten): 30
+                Euro
+              </li>
+              <li>
+                ausschließlich Inanspruchnahme der Flutlichtanlage pro Spiel
+                oder Trainingseinheit (90 Minuten): 10 Euro für Großspielfeld //
+                5 Euro für Kleinspielfeld
+              </li>
+            </ul>
           </v-col>
           <v-col cols="12" class="subtitle-1">
-            <div class="font-weight-bold">
-              Zusätzliche Kosten:
-            </div>
             <div>
-              Zahlungen von Aufwandspauschalen und Versorgung von
-              Schiedsrichtern sind vom jeweiligen mietenden Verein zu
-              entrichten, ebenso ist dieser für die Spielanmeldung zuständig.
+              Die Platzgebühren sind unmittelbar vor der Nutzung in bar zu
+              entrichten und enthalten die gesetzliche Mehrwertsteuer. Eine
+              Quittung wird ausgestellt.
             </div>
           </v-col>
           <v-col cols="12" class="subtitle-1">
@@ -78,48 +91,59 @@
             <div>
               <ul>
                 <li>
+                  Zahlungen von Aufwandspauschalen und Versorgung von
+                  Schiedsrichtern sind vom jeweils mietenden Verein zu
+                  entrichten, ebenso ist der mietende Verein für die
+                  DFB-Spielanmeldung verantwortlich.
+                </li>
+                <li>
                   Das Aufwärmen ist jeweils eine Stunde vor Spielbeginn möglich
                   (ggf. auch auf dem Kleinspielfeld).
                 </li>
                 <li>
                   Der Kunstrasenplatz darf nur mit Nocken- oder
-                  Kunstrasenschuhen bespielt werden (keine Stollen!).
+                  Kunstrasenschuhen bespielt werden. Stollenschuhe sind nicht
+                  erlaubt.
                 </li>
                 <li>
-                  Warmlauf- und Spielball, sowie Getränke sind selbstständig zu
-                  organisieren bzw. mitzubringen.
+                  Warmlauf- und Spielbälle, sowie Getränke sind selbständig
+                  mitzubringen.
                 </li>
                 <li>
-                  Bitte informiert den SV Eutingen so früh wie möglich, wenn Ihr
-                  eine Buchungszeit absagen müsst, damit der Platz evtl. anderen
-                  Nutzern zur Verfügung gestellt werden kann.
+                  Falls eine Buchungszeit abgesagt werden muss, ist der SV
+                  Eutinge umgehend zu informieren, damit der Platz anderen Teams
+                  zur Verfügung gestellt werden kann.
                 </li>
                 <li>
                   Verursachte Schäden sind dem SV Eutingen unverzüglich
-                  mitzuteilen und können ggf. in Rechnung gestellt werden.
+                  mitzuteilen. Notwendige Reparaturen können in Rechnung
+                  gestellt werden.
                 </li>
                 <li>
-                  Die Spiele bzw. Ergebnisse (Spielbericht online) sind in
+                  Spiele bzw. Ergebnisse (Spielbericht online) sind in
                   Eigenregie zu melden. Ein Zugang zum PC kann nicht
-                  sichergestellt werden ggf. empfiehlt sich die Nutzung per
-                  Smartphone-App.
+                  sichergestellt werden, daher empfiehlt sich die Nutzung der
+                  Handy-App.
                 </li>
                 <li>
-                  Es ist möglich, dass das Sportheim nicht zu allen
-                  Nutzungszeiten geöffnet hat.
+                  Es ist möglich, dass die Gaststätte Auszeit nicht zu allen
+                  Nutzungszeiten geöffnet oder eine geschlossene Gesellschaft
+                  hat.
                 </li>
               </ul>
             </div>
           </v-col>
           <v-col cols="12" class="subtitle-1">
-            <span class="font-weight-bold">
-              ACHTUNG:
-            </span>
+            <div class="font-weight-bold">
+              Achtung
+            </div>
             <span>
-              Der SV Eutingen hält sich Änderungen / Ausfall von Buchungszeiten
-              aufgrund von Witterungsverhältnissen vor. Bitte informiert Euch
-              bei entsprechender Witterung im Vorfeld über die Verfügbarkeit des
-              Platzes (Schiedsrichter und Mannschaften sind zu informieren).
+              Der SV Eutingen 1947 e.V. hält sich Änderungen oder die
+              kurzfristige Absage von Buchungszeiten aufgrund von
+              Witterungsverhältnissen vor. Bitte informiert Euch bei
+              entsprechender Witterung im Vorfeld über die Verfügbarkeit des
+              Platzes. Schiedsrichter und Mannschaften sind dann vom mietenden
+              Verein zu informieren.
             </span>
           </v-col>
         </v-row>
