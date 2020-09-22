ea90a19fe2719087734d76ded23984f9a4a5580b
mseele <mseele@gmail.com>
2020-09-22 21:06:33 +0200

    refactor(tailwind): migrate footer
    work on #160

--- src/components/base/AppFooter.vue
+++ src/components/base/AppFooter.vue
@@ -1,46 +1,55 @@
 <template>
-  <v-footer padless>
-    <v-card flat tile width="100%" class="text-center">
-      <v-card-text>
-        <template v-for="(link, index) in links.externalItems">
-          <v-btn
+  <footer class="tw-text-gray-700 tw-body-font">
+    <div
+      class="tw-container tw-py-4 tw-px-5 tw-mx-auto tw-flex tw-flex-wrap tw-justify-center tw-space-x-3"
+    >
+      <a
+        v-for="(link, index) in links.externalItems"
+        :key="index"
+        :href="link.to"
+        target="_blank"
+        rel="noreferrer"
+        class="hover:tw-bg-sec-dark tw-p-2 tw-rounded-full tw-inline-flex tw-items-center"
+      >
+        <g-image class="tw-w-5 tw-h-5" :src="require('@/assets/' + link.img)" />
+      </a>
+    </div>
+    <div class="tw-border-solid tw-border-0 tw-border-t tw-border-sec-medium">
+      <div class="tw-container tw-mx-auto tw-py-4 tw-px-5">
+        <div class="tw-flex tw-flex-wrap tw-justify-center">
+          <div
+            v-for="(item, index) in links.footerItems"
             :key="index"
-            class="mx-4"
-            :href="link.to"
-            target="_blank"
-            rel="noreferrer"
-            small
-            text
-            icon
+            class="tw-p-2"
           >
-            <v-avatar size="20">
-              <v-img :src="require('@/assets/' + link.img)"></v-img>
-            </v-avatar>
-          </v-btn>
-        </template>
-      </v-card-text>
-      <v-row justify="center" class="px-2 pb-4 subtitle-2" no-gutters>
-        <template v-for="(item, index) in links.footerItems">
-          <g-link :key="index" :to="item.to" class="px-2 lnk">{{
-            item.title
-          }}</g-link>
-        </template>
-      </v-row>
-      <v-divider></v-divider>
-      <v-card-text class="black--text">
-        &copy; {{ new Date().getFullYear() }} —
-        <strong>SV Eutingen 1947 e.V. </strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+            <g-link
+              :key="index"
+              :to="item.to"
+              class="tw-text-black tw-text-sm tw-font-medium tw-no-underline"
+            >
+              {{ item.title }}
+            </g-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="tw-bg-sec-medium">
+      <div
+        class="tw-container tw-mx-auto tw-py-4 tw-px-5 tw-flex tw-flex-wrap tw-flex-col sm:tw-flex-row"
+      >
+        <div class="tw-text-gray-900 tw-text-sm tw-text-center sm:tw-text-left">
+          © {{ new Date().getFullYear() }} —
+          <span class="tw-font-bold">SV Eutingen 1947 e.V.</span>
+        </div>
+        <span
+          class="tw-text-primary tw-font-bold sm:tw-ml-auto sm:tw-mt-0 tw-mt-2 sm:tw-w-auto tw-w-full sm:tw-text-left tw-text-center tw-text-sm"
+        >
+          #mehralseinverein
+        </span>
+      </div>
+    </div>
+  </footer>
 </template>
-
-<style lang="scss">
-.lnk {
-  color: black !important;
-  text-decoration: none !important;
-}
-</style>
 
 <script>
 import links from '@/data/links.json'
