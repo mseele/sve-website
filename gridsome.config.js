c5c229442e9b188379a2376f4f57310ee6daeada
mseele <mseele@gmail.com>
2020-09-23 19:47:54 +0200

    fix(tailwind): remove PurgeCSS in favor of Tailwind's built-in Purge

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,11 +1,6 @@
 const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
 
 const postcssPlugins = [tailwind()]
-
-if (process.env.NODE_ENV === 'production') {
-  postcssPlugins.push(purgecss(require('./purgecss.config.js')))
-}
 
 module.exports = {
   siteName: 'SV Eutingen 1947 e.V.',
