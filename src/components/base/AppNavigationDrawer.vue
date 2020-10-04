376f3188403ad4ea75a36eec8c559ede47718a84
mseele <mseele@gmail.com>
2020-10-04 14:11:53 +0200

    refactor(navigation drawer): improve navigation drawer width over the different breakpoints

--- src/components/base/AppNavigationDrawer.vue
+++ src/components/base/AppNavigationDrawer.vue
@@ -32,7 +32,7 @@
       <div
         v-if="drawer"
         class="tw-z-20 tw-fixed tw-inset-0 tw-overflow-hidden"
-        :class="{ 'ml-10': drawer }"
+        :class="{ 'tw-ml-14': drawer }"
         @click="drawer = false"
       >
         <div class="tw-absolute tw-inset-0 tw-overflow-hidden">
@@ -40,9 +40,11 @@
             class="tw-absolute tw-inset-y-0 tw-right-0 tw-max-w-full tw-flex"
             @click.stop
           >
-            <div class="tw-relative">
+            <div
+              class="tw-relative tw-w-screen tw-max-w-xs lg:tw-max-w-sm xl:tw-max-w-md"
+            >
               <div
-                class="tw-h-full tw-flex tw-flex-col tw-border-solid tw-border-0 tw-border-t-2 tw-border-red-800 tw-space-y-2 xl:tw-space-y-4 tw-px-2 xl:tw-px-4 tw-py-2 xl:tw-py-4 tw-bg-white tw-shadow-xl tw-overflow-y-scroll"
+                class="tw-h-full tw-flex tw-flex-col tw-space-y-2 xl:tw-space-y-4 tw-p-2 xl:tw-p-4 tw-bg-white tw-shadow-xl tw-overflow-y-scroll"
               >
                 <external-links class="tw-px-4 md:tw-px-6 tw-mx-auto" />
                 <div
@@ -53,7 +55,7 @@
                     v-for="(item, index) in items"
                     :key="'itm' + index"
                     :to="$static.metadata.pathPrefix + item.to"
-                    class="tw-block tw-my-1 tw-py-2 tw-pl-2 tw-pr-10 xl:tw-py-3 xl:tw-pl-3 xl:tw-pr-20 tw-rounded-md tw-text-gray-800 hover:tw-text-black tw-text-sm xl:tw-text-base tw-font-medium tw-no-underline"
+                    class="tw-block tw-my-1 tw-p-2 xl:tw-p-3 tw-rounded-md tw-text-gray-800 hover:tw-text-black tw-text-sm xl:tw-text-base tw-font-medium tw-no-underline"
                     exact-active-class="tw-bg-gray-300 tw-text-black"
                     >{{ item.title }}</g-link
                   >
