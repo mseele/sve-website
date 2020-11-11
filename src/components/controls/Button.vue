84d936361f5f37ceea3d946364322b2df086f07f
mseele <mseele@gmail.com>
2020-11-11 20:02:03 +0100

    feat(controls): add button and title components

---
+++ src/components/controls/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <a
+    class="tw-px-4 tw-py-2 tw-font-medium tw-tracking-widest tw-no-underline tw-uppercase tw-whitespace-no-wrap tw-rounded-full focus:tw-outline-none focus:tw-shadow-outline"
+    :class="[
+      outlined
+        ? 'tw-text-white tw-border tw-border-white tw-bg-opacity-25 tw-border-solid tw-bg-white hover:tw-bg-gray-400 hover:tw-bg-opacity-25 active:tw-bg-gray-500'
+        : 'tw-text-gray-800 tw-bg-white tw-border tw-border-white tw-border-solid hover:tw-bg-gray-400 active:tw-bg-gray-500',
+    ]"
+    :href="href"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: null,
+      required: true,
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+}
+</script>
---
+++ src/components/controls/HeaderTitle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="tw-flex tw-flex-row">
+    <div class="tw-w-1 tw-h-auto tw-bg-red-800 tw-rounded"></div>
+    <div
+      class="tw-pl-2 tw-leading-none tw-whitespace-no-wrap"
+      :class="[
+        dark ? 'tw-text-gray-900' : 'tw-text-white',
+        shadow ? (dark ? 'tw-text-shadow-light' : 'tw-text-shadow-dark') : '',
+        uppercase ? 'tw-uppercase tw-tracking-widest' : '',
+      ]"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dark: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    shadow: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    uppercase: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+}
+</script>
