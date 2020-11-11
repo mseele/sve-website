a60b1204cff38c39f7bbb5548664aa77bd602c04
mseele <mseele@gmail.com>
2020-11-11 20:21:31 +0100

    feat(button): add dark mode support

--- src/components/controls/Button.vue
+++ src/components/controls/Button.vue
@@ -1,10 +1,15 @@
 <template>
   <a
-    class="tw-px-4 tw-py-2 tw-font-medium tw-tracking-widest tw-no-underline tw-uppercase tw-whitespace-no-wrap tw-rounded-full focus:tw-outline-none focus:tw-shadow-outline"
+    class="tw-px-4 tw-py-2 tw-font-medium tw-tracking-widest tw-no-underline tw-uppercase tw-whitespace-no-wrap tw-border tw-border-solid tw-rounded-full focus:tw-outline-none focus:tw-shadow-outline"
     :class="[
       outlined
-        ? 'tw-text-white tw-border tw-border-white tw-bg-opacity-25 tw-border-solid tw-bg-white hover:tw-bg-gray-400 hover:tw-bg-opacity-25 active:tw-bg-gray-500'
-        : 'tw-text-gray-800 tw-bg-white tw-border tw-border-white tw-border-solid hover:tw-bg-gray-400 active:tw-bg-gray-500',
+        ? 'tw-bg-opacity-25 hover:tw-bg-opacity-25 ' +
+          (dark
+            ? 'tw-text-black tw-border-black tw-bg-gray-900 hover:tw-bg-gray-800 active:tw-bg-gray-700'
+            : 'tw-text-white tw-border-white tw-bg-white hover:tw-bg-gray-400 active:tw-bg-gray-500')
+        : dark
+        ? 'tw-text-white tw-bg-gray-900 tw-border-gray-900 hover:tw-border-gray-800 hover:tw-bg-gray-800 active:tw-bg-gray-700'
+        : 'tw-text-gray-900 tw-bg-white tw-border-white hover:tw-bg-gray-400 active:tw-bg-gray-500',
     ]"
     :href="href"
   >
@@ -21,6 +26,11 @@ export default {
       required: true,
     },
     outlined: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    dark: {
       type: Boolean,
       default: false,
       required: false,
