500a523abc049036974fdf3513379e25539cf91f
mseele <mseele@gmail.com>
2020-11-14 18:58:58 +0100

    feat(section): add new page section

---
+++ src/components/common/PageSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="[dark ? 'tw-bg-gray-300' : 'tw-bg-gray-100']">
+    <div
+      class="tw-container tw-p-4 tw-py-10 tw-mx-auto sm:tw-px-10 md:tw-px-20"
+    >
+      <header-title
+        dark
+        class="tw-text-3xl tw-font-semibold tw-tracking-widest tw-uppercase"
+      >
+        {{ title }}
+      </header-title>
+      <div class="tw-pt-6">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import headerTitle from '@/components/controls/HeaderTitle'
+
+export default {
+  components: {
+    headerTitle,
+  },
+  props: {
+    title: {
+      type: String,
+      default: null,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
