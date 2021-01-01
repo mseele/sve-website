c27fd4784856e68f1e6e6d7cdbe15ae7331daef3
mseele <mseele@gmail.com>
2021-01-01 19:58:00 +0100

    refactor(sponsoring): migrate to tailwindcss

--- src/pages/sponsoring.vue
+++ src/pages/sponsoring.vue
@@ -9,156 +9,165 @@
     >
       <g-image src="@/assets/sponsoring/main.jpg" :class="imageClass" />
     </hero-section>
-    <section class="section">
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <h2>SPONSORING</h2>
-          </v-col>
-          <v-col cols="12" class="subtitle-1">
-            Unsere Sponsoren und Partner ermöglichen durch ihre finanzielle und
-            materielle Unterstützung das sportliche und kulturelle Angebot des
-            SV Eutingen. Durch solche Kooperationen ist es möglich, ein
-            ehrenamtlich breites Sportangebot auf die Beine zu stellen. Deshalb
-            fühlen wir uns unseren Sponsoren gegenüber verpflichtet und nehmen
-            deren Dienstleistungen und Produkte in Anspruch. Wenn ein besonderes
-            Firmenevent, wie bspw. ein Jubiläum ins Haus steht, packen wir auch
-            gerne tatkräftig mit an.<br />
-            Die Möglichkeiten einer Sponsoringpartnerschaft sind vielfältig:
-            Sponsoren können bspw. direkt in die Aus- und Weiterbildung unserer
-            Jugendtrainer investieren oder für ein bestimmtes Team auf deren
-            Trikots oder sonstigem Equipment Werbung machen. Hast auch Du
-            Interesse an einer Partnerschaft? Dann wende Dich bitte über das
-            <g-link
-              :to="{
-                path: '/kontakt/',
-                query: { auswahl: 'sponsoring', thema: 'sponsoring' },
-              }"
-              >Kontaktformular</g-link
-            >
-            an unseren Vorsitzenden Sebastian Lazar, der für Gespräche gerne zur
-            Verfügung steht.
-          </v-col>
-        </v-row>
-      </v-container>
-    </section>
-    <section id="bande" class="section_alt">
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <h2>BANDENWERBUNG</h2>
-          </v-col>
-          <v-col cols="12" class="subtitle-1">
-            Außerdem bieten wir mit unserem Partner der Eutinger Sportplatz- und
-            Vereinswerbung GbR seit über 20 Jahren Bandenwerbung auf unserer
-            sehr belebten Sportanlage. Die Abwicklung läuft unkompliziert. Wir
-            kümmern uns über Anschaffung und Pflege der Bandenwerbung.
-            <br />
-            Bei Interesse stehen Dir über das
-            <g-link
-              :to="{
-                path: '/kontakt/',
-                query: { auswahl: 'sponsoring', thema: 'bande' },
-              }"
-              >Kontaktformular</g-link
-            >
-            Franz Nesch und Thomas Akermann gerne zur Verfügung.
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-row justify="center" no-gutters>
-              <v-btn
-                text
-                rounded
-                color="primary"
-                href="/downloads/2020/Vertrag-Bandenwerbung.pdf"
-                target="_blank"
-              >
-                <v-icon left>{{ mdiCloudDownload }}</v-icon
-                >Vertrag Bandenwerbung</v-btn
-              >
-            </v-row>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-row justify="center" no-gutters>
-              <v-btn
-                text
-                rounded
-                color="primary"
-                href="/downloads/2020/Preise-Bandenwerbung.pdf"
-                target="_blank"
-              >
-                <v-icon left>{{ mdiCloudDownload }}</v-icon
-                >Preise Bandenwerbung</v-btn
-              >
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-container>
-    </section>
-    <section id="partner" class="section">
-      <v-container>
-        <v-row class="pb-2">
-          <v-col cols="12">
-            <h2>UNSERE PARTNER</h2>
-          </v-col>
-          <v-col cols="12" class="subtitle-1">
-            Mit diesen langjährigen Partnern leben wir Zusammenarbeit. Durch
-            ihre materielle und finanzielle Unterstützung ermöglichen sie unser
-            ehrenamtliches Angebot und machen auf ihre Produkte und
-            Dienstleistungen aufmerksam. Partnerschaft bedeutet für uns, dass
-            beide Seiten profitieren. Klickt auf die Namen und gelangt direkt
-            auf die Homepage unserer Partner.
-          </v-col>
-        </v-row>
-        <v-row
-          v-for="(key, groupIndex) in Object.keys(sponsors)"
-          :key="groupIndex"
-          :class="{ 'pt-6 pt-sm-8 pt-lg-10': groupIndex > 0 }"
+    <page-section title="Sponsoring">
+      Unsere Sponsoren und Partner ermöglichen durch ihre finanzielle und
+      materielle Unterstützung das sportliche und kulturelle Angebot des SV
+      Eutingen. Durch solche Kooperationen ist es möglich, ein ehrenamtlich
+      breites Sportangebot auf die Beine zu stellen. Deshalb fühlen wir uns
+      unseren Sponsoren gegenüber verpflichtet und nehmen deren Dienstleistungen
+      und Produkte in Anspruch. Wenn ein besonderes Firmenevent, wie bspw. ein
+      Jubiläum ins Haus steht, packen wir auch gerne tatkräftig mit an.<br />
+      Die Möglichkeiten einer Sponsoringpartnerschaft sind vielfältig: Sponsoren
+      können bspw. direkt in die Aus- und Weiterbildung unserer Jugendtrainer
+      investieren oder für ein bestimmtes Team auf deren Trikots oder sonstigem
+      Equipment Werbung machen. Hast auch Du Interesse an einer Partnerschaft?
+      Dann wende Dich bitte über das
+      <g-link
+        class="tw-rounded tw-on-focus-dark"
+        :to="{
+          path: '/kontakt/',
+          query: { auswahl: 'sponsoring', thema: 'sponsoring' },
+        }"
+        >Kontaktformular</g-link
+      >
+      an unseren Vorsitzenden Sebastian Lazar, der für Gespräche gerne zur
+      Verfügung steht.
+    </page-section>
+    <page-section id="bande" title="Bandenwerbung" dark>
+      Außerdem bieten wir mit unserem Partner der Eutinger Sportplatz- und
+      Vereinswerbung GbR seit über 20 Jahren Bandenwerbung auf unserer sehr
+      belebten Sportanlage. Die Abwicklung läuft unkompliziert. Wir kümmern uns
+      über Anschaffung und Pflege der Bandenwerbung.
+      <br />
+      Bei Interesse stehen Dir über das
+      <g-link
+        class="tw-rounded tw-on-focus-dark"
+        :to="{
+          path: '/kontakt/',
+          query: { auswahl: 'sponsoring', thema: 'bande' },
+        }"
+        >Kontaktformular</g-link
+      >
+      Franz Nesch und Thomas Akermann gerne zur Verfügung.
+      <div
+        class="tw-flex tw-flex-col tw-pt-6 md:tw-flex-row md:tw-justify-center"
+      >
+        <a
+          class="tw-inline-flex tw-items-center tw-px-5 tw-py-3 tw-text-black tw-no-underline tw-bg-white tw-rounded-lg hover:tw-bg-gray-100 tw-on-focus-dark"
+          href="/downloads/2020/Vertrag-Bandenwerbung.pdf"
+          target="_blank"
         >
-          <v-col cols="12">
-            <h3>{{ key }}</h3>
-          </v-col>
-          <v-col
+          <svg
+            class="tw-w-8 tw-h-8"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <span
+            class="tw-flex tw-flex-col tw-items-start tw-ml-4 tw-leading-none"
+          >
+            <span class="tw-mb-1 tw-text-xs tw-text-gray-600">VERTRAG</span>
+            <span class="tw-font-medium tw-title-font">Bandenwerbung</span>
+          </span>
+        </a>
+        <a
+          class="tw-inline-flex tw-items-center tw-px-5 tw-py-3 tw-mt-4 tw-text-black tw-no-underline tw-bg-white tw-rounded-lg md:tw-ml-4 lg:tw-ml-8 2xl:tw-ml-20 md:tw-mt-0 hover:tw-bg-gray-100 tw-on-focus-dark"
+          href="/downloads/2020/Preise-Bandenwerbung.pdf"
+          target="_blank"
+        >
+          <svg
+            class="tw-w-8 tw-h-8"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.736 6.979C9.208 6.193 9.696 6 10 6c.304 0 .792.193 1.264.979a1 1 0 001.715-1.029C12.279 4.784 11.232 4 10 4s-2.279.784-2.979 1.95c-.285.475-.507 1-.67 1.55H6a1 1 0 000 2h.013a9.358 9.358 0 000 1H6a1 1 0 100 2h.351c.163.55.385 1.075.67 1.55C7.721 15.216 8.768 16 10 16s2.279-.784 2.979-1.95a1 1 0 10-1.715-1.029c-.472.786-.96.979-1.264.979-.304 0-.792-.193-1.264-.979a4.265 4.265 0 01-.264-.521H10a1 1 0 100-2H8.017a7.36 7.36 0 010-1H10a1 1 0 100-2H8.472c.08-.185.167-.36.264-.521z"
+              clip-rule="evenodd"
+            />
+          </svg>
+
+          <span
+            class="tw-flex tw-flex-col tw-items-start tw-ml-4 tw-leading-none"
+          >
+            <span class="tw-mb-1 tw-text-xs tw-text-gray-600">PREISE</span>
+            <span class="tw-font-medium tw-title-font">Bandenwerbung</span>
+          </span>
+        </a>
+      </div>
+    </page-section>
+    <page-section id="partner" title="Unsere Partner">
+      <div class="tw--mb-4">
+        Mit diesen langjährigen Partnern leben wir Zusammenarbeit. Durch ihre
+        materielle und finanzielle Unterstützung ermöglichen sie unser
+        ehrenamtliches Angebot und machen auf ihre Produkte und Dienstleistungen
+        aufmerksam. Partnerschaft bedeutet für uns, dass beide Seiten
+        profitieren. Klickt auf die Namen und gelangt direkt auf die Homepage
+        unserer Partner.
+      </div>
+      <div
+        v-for="(key, groupIndex) in Object.keys(sponsors)"
+        :key="groupIndex"
+        class="tw-pt-8"
+      >
+        <header-title dark class="tw-text-xl tw-font-medium">
+          {{ key }}
+        </header-title>
+        <div class="tw-flex tw-flex-wrap lg:tw--ml-4">
+          <div
             v-for="(item, index) of sponsors[key]"
             :key="index"
-            class="py-0"
-            cols="12"
-            lg="6"
+            class="tw-w-full tw-pt-4 lg:tw-pl-4 lg:tw-w-1/2"
           >
-            <ul>
-              <li class="font-weight-medium">
-                <a
-                  v-if="item.url"
-                  :href="item.url"
-                  class="sponsor"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  {{ item.name }}
-                </a>
-                <span v-else>{{ item.name }}</span>
-              </li>
-            </ul>
-          </v-col>
-        </v-row>
-      </v-container>
-    </section>
+            <component
+              :is="item.url ? 'a' : 'div'"
+              :href="item.url"
+              target="_blank"
+              rel="noopener"
+              class="tw-flex tw-items-center tw-h-full tw-p-4 tw-text-black tw-no-underline tw-bg-white tw-rounded tw-on-focus-dark"
+              :class="{ 'hover:tw-bg-gray-200': item.url }"
+            >
+              <svg
+                class="tw-flex-shrink-0 tw-w-6 tw-h-6 tw-mr-4 tw-text-red-800"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <span class="tw-font-medium tw-title-font">{{ item.name }}</span>
+            </component>
+          </div>
+        </div>
+      </div>
+    </page-section>
   </Layout>
 </template>
 
 <script>
 import heroSection from '@/components/common/HeroSection'
-import { mdiCloudDownload } from '@mdi/js'
+import pageSection from '@/components/common/PageSection'
+import headerTitle from '@/components/controls/HeaderTitle'
 import sponsoring from '@/data/sponsoring.json'
 
 export default {
   metaInfo: {
     title: 'Sponsoring',
   },
-  components: { heroSection },
+  components: { heroSection, pageSection, headerTitle },
   data() {
     return {
-      mdiCloudDownload,
       sponsors: this.groupSponsors(sponsoring),
     }
   },
