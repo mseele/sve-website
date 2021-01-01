3b901bc9e0f5f6bdade879478ebc4c65f95460f6
mseele <mseele@gmail.com>
2021-01-01 18:57:01 +0100

    refactor(newsletter): migrate validation to vee-validate

--- src/components/common/EmailSubscription.vue
+++ src/components/common/EmailSubscription.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="tw-flex tw-flex-col">
-    <div class="tw-flex tw-flex-col lg:tw-flex-row lg:tw-items-center">
+    <ValidationProvider
+      v-slot="{ errors }"
+      rules="required|email"
+      class="tw-flex tw-flex-col lg:tw-flex-row lg:tw-items-center"
+      tag="div"
+    >
       <input
         v-model="email"
         type="email"
@@ -10,11 +15,11 @@
       <btn
         class="tw-mt-2 lg:tw-mt-0 lg:tw-ml-2"
         :loading="loading"
-        @click="onClick()"
+        @click="onClick(errors)"
       >
         {{ isSubscription() ? 'Infos erhalten' : 'Abmelden' }}
       </btn>
-    </div>
+    </ValidationProvider>
     <div class="tw-pt-2 tw-text-sm tw-text-gray-800">
       Wir nehmen Datenschutz sehr ernst. Informiere dich in unserer
       <a class="tw-text-black tw-rounded tw-on-focus-dark" href="/datenschutz"
@@ -26,11 +31,22 @@
 
 <script>
 import axios from 'axios'
-import { validateEmail } from '@/util/validators'
+import { ValidationProvider, extend } from 'vee-validate'
+import { required, email } from 'vee-validate/dist/rules'
 import btn from '@/components/controls/PrimaryButton'
 
+extend('required', {
+  ...required,
+  message: 'Eine E-Mail Adresse wird benötigt',
+})
+
+extend('email', {
+  ...email,
+  message: 'Die E-Mail Adresse ist ungültig',
+})
+
 export default {
-  components: { btn },
+  components: { ValidationProvider, btn },
   props: {
     successMessage: {
       type: String,
@@ -55,86 +71,70 @@ export default {
     isSubscription() {
       return this.type === 'subscribe'
     },
-    onClick() {
-      if (this.isSubscription()) {
-        this.subscribe()
-      } else {
-        this.unsubscribe()
-      }
-    },
-    unsubscribe() {
-      const value = this.email.trim()
+    onClick(errors) {
       if (this.newsTypes.length === 0) {
         this.$store.commit(
           'notification_showError',
           'Bitte wähle mindestens eine Newletter Option aus. Vielen Dank.'
         )
-      } else if (!validateEmail(value)) {
-        this.$store.commit(
-          'notification_showError',
-          'Bitte gib eine gültige E-Mail Addresse an. Vielen Dank.'
-        )
+      } else if (errors.length > 0) {
+        this.$store.commit('notification_showError', errors[0])
       } else {
-        this.loading = true
-        const data = {
-          email: value,
-          types: this.newsTypes,
+        const value = this.email.trim()
+        if (this.isSubscription()) {
+          this.subscribe(value)
+        } else {
+          this.unsubscribe(value)
         }
-        axios
-          .post(this.$static.metadata.newsAPI + '/unsubscribe', data)
-          .then((response) => {
-            this.loading = false
-            this.email = ''
-            this.$store.commit('notification_showInfo', this.successMessage)
-          })
-          .catch((error) => {
-            // eslint-disable-next-line no-console
-            console.log(error)
-            this.loading = false
-            this.email = ''
-            this.$store.commit(
-              'notification_showError',
-              'Leider ist etwas schief gelaufen. Bitte versuche es später noch einmal.'
-            )
-          })
       }
     },
-    subscribe() {
-      const value = this.email.trim()
-      if (this.newsTypes.length === 0) {
-        this.$store.commit(
-          'notification_showError',
-          'Bitte wähle mindestens eine Newletter Option aus. Vielen Dank.'
-        )
-      } else if (!validateEmail(value)) {
-        this.$store.commit(
-          'notification_showError',
-          'Bitte gib eine gültige E-Mail Addresse an. Vielen Dank.'
-        )
-      } else {
-        this.loading = true
-        const data = {
-          email: value,
-          types: this.newsTypes,
-        }
-        axios
-          .post(this.$static.metadata.newsAPI + '/subscribe', data)
-          .then((response) => {
-            this.loading = false
-            this.email = ''
-            this.$store.commit('notification_showInfo', this.successMessage)
-          })
-          .catch((error) => {
-            // eslint-disable-next-line no-console
-            console.log(error)
-            this.loading = false
-            this.email = ''
-            this.$store.commit(
-              'notification_showError',
-              'Leider ist etwas schief gelaufen. Bitte versuche es später noch einmal.'
-            )
-          })
+    unsubscribe(email) {
+      this.loading = true
+      const data = {
+        email: email,
+        types: this.newsTypes,
       }
+      axios
+        .post(this.$static.metadata.newsAPI + '/unsubscribe', data)
+        .then((response) => {
+          this.loading = false
+          this.email = ''
+          this.$store.commit('notification_showInfo', this.successMessage)
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error)
+          this.loading = false
+          this.email = ''
+          this.$store.commit(
+            'notification_showError',
+            'Leider ist etwas schief gelaufen. Bitte versuche es später noch einmal.'
+          )
+        })
+    },
+    subscribe(value) {
+      this.loading = true
+      const data = {
+        email: value,
+        types: this.newsTypes,
+      }
+      axios
+        .post(this.$static.metadata.newsAPI + '/subscribe', data)
+        .then((response) => {
+          this.loading = false
+          this.email = ''
+          this.$store.commit('notification_showInfo', this.successMessage)
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error)
+          this.loading = false
+          this.email = ''
+          this.$store.commit(
+            'notification_showError',
+            'Leider ist etwas schief gelaufen. Bitte versuche es später noch einmal.'
+          )
+        })
     },
   },
 }
