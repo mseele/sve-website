242c1e2594aef058462010b5ea78b899e464136f
mseele <mseele@gmail.com>
2021-01-09 21:03:24 +0100

    refactor(email-subscription): improve spacing between

--- src/components/events/emailSubscription.vue
+++ src/components/events/emailSubscription.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="flex flex-col lg:items-center lg:flex-row lg:justify-between">
-    <div class="pb-3 lg:pb-0 lg:pr-3">
+    <div class="w-full pb-4 lg:w-1/2 lg:pb-0 lg:pr-2">
       <div class="text-xl font-medium">Halte dich auf dem Laufenden.</div>
       <div class="pt-1 lg:pt-2">
         <slot></slot>
       </div>
     </div>
-    <emailSubscription
+    <email-subscription
       :success-message="subscribeSuccess"
       :news-types="[newsType]"
+      class="w-full lg:w-1/2 lg:pl-2"
     />
   </div>
 </template>
