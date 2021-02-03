8277716513c5f49993f3d73eda5f7ef1b55aee71
mseele <mseele@gmail.com>
2021-02-03 20:09:35 +0100

    refactor(converter): improve if clause order

--- src/util/converters.js
+++ src/util/converters.js
@@ -1,10 +1,10 @@
 export function toSubscribers(value) {
   if (value === -1) {
     return 'Freie Plätze verfügbar'
-  } else if (value > 1) {
-    return 'Noch ' + value + ' freie Plätze'
   } else if (value === 1) {
     return 'Noch 1 freier Platz'
+  } else if (value > 1) {
+    return 'Noch ' + value + ' freie Plätze'
   }
   return 'Die Warteliste ist geöffnet'
 }
