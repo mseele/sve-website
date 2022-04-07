11a2a29739aa45de5f6856ff151321485e875997
Michael Seele <mseele@gmail.com>
2022-04-07 20:13:18 +0200

    chore: move image cache out of node_modules
    because if not it will be deleted each yarn install

--- iles.config.ts
+++ iles.config.ts
@@ -5,26 +5,29 @@ import svgLoader from 'vite-svg-loader'
 export default defineConfig({
   siteUrl: 'https://www.sv-eutingen.de',
   modules: [
-    images({
-      full: hdPreset({
-        widths: [400, 800, 1200, 1800, 2400, 3000],
-        sizes: '100vw',
-        formats: {
-          // avif: { quality: 75 },
-          webp: { quality: 75 },
-          jpg: { quality: 75 },
-        },
-      }),
-      card: hdPreset({
-        widths: [200, 400, 800, 1200, 1800],
-        sizes: '(min-width: 1536px) 30vw, (min-width: 1024px) 50vw, 100vw',
-        formats: {
-          // avif: { quality: 55 },
-          webp: { quality: 55 },
-          jpg: { quality: 55 },
-        },
-      }),
-    }),
+    images(
+      {
+        full: hdPreset({
+          widths: [400, 800, 1200, 1800, 2400, 3000],
+          sizes: '100vw',
+          formats: {
+            // avif: { quality: 75 },
+            webp: { quality: 75 },
+            jpg: { quality: 75 },
+          },
+        }),
+        card: hdPreset({
+          widths: [200, 400, 800, 1200, 1800],
+          sizes: '(min-width: 1536px) 30vw, (min-width: 1024px) 50vw, 100vw',
+          formats: {
+            // avif: { quality: 55 },
+            webp: { quality: 55 },
+            jpg: { quality: 55 },
+          },
+        }),
+      },
+      { cacheDir: '.cache/images' }
+    ),
   ],
   vite: {
     plugins: [svgLoader()],
