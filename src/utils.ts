da2a33c2ed277ef83df5f1ea09b573ef3abcfa24
Michael Seele <mseele@gmail.com>
2024-01-05 09:28:16 +0100

    chore: bump dependencies

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 import { format } from 'date-fns'
-// @ts-expect-error: without the index.js suffix, astro fails to compile
-import { de } from 'date-fns/locale/index.js'
+import { de } from 'date-fns/locale'
 
 export function formatDate(date: Date) {
   return format(date, 'd. MMMM yyyy', { locale: de })
