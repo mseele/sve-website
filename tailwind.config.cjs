baa8112ba6121cc4ebc7d26fc805f1bef62ce41a
Michael Seele <mseele@gmail.com>
2024-08-20 21:17:07 +0200

    feat: add notification component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -116,6 +116,8 @@ module.exports = {
       spin: 'spin 1s linear infinite',
       ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
       pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      'slide-up': 'slide-up 0.3s ease-out forwards',
+      'slide-right': 'slide-right 0.3s ease-out forwards',
       bounce: 'bounce 1s infinite'
     },
     aspectRatio: {
@@ -584,6 +586,26 @@ module.exports = {
         '50%': {
           transform: 'none',
           animationTimingFunction: 'cubic-bezier(0,0,0.2,1)'
+        }
+      },
+      'slide-up': {
+        '0%': {
+          transform: 'translateY(100%)',
+          opacity: 0
+        },
+        '100%': {
+          transform: 'translateY(0)',
+          opacity: 1
+        }
+      },
+      'slide-right': {
+        '0%': {
+          transform: 'translateX(0)',
+          opacity: 1
+        },
+        '100%': {
+          transform: 'translateX(100%)',
+          opacity: 0
         }
       }
     },
