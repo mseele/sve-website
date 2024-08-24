fffdec187a832dd8e499b74497dea4a783adca15
Michael Seele <mseele@gmail.com>
2024-08-24 19:50:43 +0200

    feat: add newsletter page

---
+++ src/api/newsletter.ts
@@ -0,0 +1,24 @@
+export async function manageSubscription(
+  subscribe: boolean,
+  email: string,
+  general: boolean,
+  fitness: boolean,
+  events: boolean
+): Promise<Response> {
+  const url = `${import.meta.env.PUBLIC_BACKEND_API}/news/${subscribe ? 'subscribe' : 'unsubscribe'}`
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ email, types: newsTypes(general, fitness, events) }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+function newsTypes(general: boolean, fitness: boolean, events: boolean) {
+  const types: string[] = []
+  if (general) types.push('General')
+  if (fitness) types.push('Fitness')
+  if (events) types.push('Events')
+  return types
+}
