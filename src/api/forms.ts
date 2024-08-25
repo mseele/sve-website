92a8bd92861f489d510d3757859ae0a6e6893576
Michael Seele <mseele@gmail.com>
2024-08-25 14:10:47 +0200

    feat: add contact page

--- src/api/forms.ts
+++ src/api/forms.ts
@@ -1,5 +1,29 @@
 import { BACKEND_API } from 'astro:env/client'
 
+export async function sendContactMessage(
+  recipient: string,
+  name: string,
+  email: string,
+  phone: string | undefined,
+  message: string
+): Promise<Response> {
+  const url = `${BACKEND_API}/contact/message`
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      type: 'General',
+      to: recipient,
+      name: name.trim(),
+      email: email.trim(),
+      phone: phone ? phone.trim() : undefined,
+      message: message.trim()
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
 export async function manageSubscription(
   subscribe: boolean,
   email: string,
