6d452c8dafb43635f13e561c0b8c923b04ae7b4d
Michael Seele <mseele@gmail.com>
2024-09-28 21:53:05 +0200

    refactor: move external links into data file

---
+++ src/data/links.ts
@@ -0,0 +1,32 @@
+export const external = [
+  {
+    to: 'https://www.instagram.com/sveutingen1947',
+    title: 'Instagram',
+    icon: 'mdi:instagram'
+  },
+  {
+    to: 'https://www.facebook.com/sveutingen',
+    title: 'Facebook',
+    icon: 'mdi:facebook'
+  },
+  {
+    to: 'https://www.whatsapp.com/channel/0029VaIzBnnE50Uj9Pflls1t',
+    title: 'WhatsApp',
+    icon: 'mdi:whatsapp'
+  },
+  {
+    to: 'https://www.youtube.com/@SVEutingeneV',
+    title: 'Youtube',
+    icon: 'mdi:youtube'
+  },
+  {
+    to: 'https://www.linkedin.com/company/sv-eutingen-1947-e-v/',
+    title: 'LinkedIn',
+    icon: 'mdi:linkedin'
+  },
+  {
+    to: 'http://www.fussball.de/verein/sv-eutingen-wuerttemberg/-/id/00ES8GNAUG000068VV0AG08LVUPGND5I',
+    title: 'Fussball.de',
+    icon: 'fussball_de'
+  }
+]
