5fda883a0df63d28d262d9ca7fe381b38c25bccf
Michael Seele <mseele@gmail.com>
2024-09-29 20:14:54 +0200

    chore: remove unused import

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
